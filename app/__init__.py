7405a5a310319a4ae26a2ec81ab810a781dcf9db
lohjine <66872975+lohjine@users.noreply.github.com>
2020-09-09 19:18:50 +0800

    Changed to rotating file handler

--- app/__init__.py
+++ app/__init__.py
@@ -62,8 +62,8 @@ def create_app(extra_config_settings={}):
 
     # Additional persistent(not streamed) error log
     import logging
-    from logging import Formatter, FileHandler
-    file_handler = FileHandler('logs/flask_app_error.log')
+    from logging import Formatter, RotatingFileHandler
+    file_handler = RotatingFileHandler('logs/flask_app_error.log', maxBytes=1e9)
     file_handler.setLevel(logging.ERROR)
     file_handler.setFormatter(
         Formatter(f'%(asctime)s %(levelname)s : %(message)s'))
